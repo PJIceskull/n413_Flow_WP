5cebb1b676f0208789a13301182de41e9a70bc8c
Pierce Issah <PJIceskull2003@gmail.com>
2025-03-10 16:26:40 -0400

    init files to github repo

---
+++ wp-content/themes/workflow_mid/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function init_editor_style()
+{
+    add_editor_style("style.css");
+}
+
+add_action("after_setup_theme", "init_editor_style");
+
+function init_view_style()
+{
+    wp_enqueue_style("workflow_mid_style", get_stylesheet_directory_uri() . "style.css");
+}
+
+add_action("wp_enqueue_style", "init_view_style");
