fe75976436f0bbdc3d4d7a7f3e9287b198b11673
Pierce Issah <PJIceskull2003@gmail.com>
2025-03-10 16:45:13 -0400

    fixed issue with styles not displaying on published page

--- wp-content/themes/workflow_mid/functions.php
+++ wp-content/themes/workflow_mid/functions.php
@@ -9,7 +9,7 @@ add_action("after_setup_theme", "init_editor_style");
 
 function init_view_style()
 {
-    wp_enqueue_style("workflow_mid_style", get_stylesheet_directory_uri() . "style.css");
+    wp_enqueue_style("workflow_mid_style", get_stylesheet_directory_uri() . "/style.css");
 }
 
-add_action("wp_enqueue_style", "init_view_style");
+add_action("wp_enqueue_scripts", "init_view_style");
