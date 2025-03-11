e7c2ecc2c7ac43731b97e200d76723e4a62c01f5
Pierce Issah <PJIceskull2003@gmail.com>
2025-03-11 17:34:55 -0400

    created custom block for feature block

--- wp-content/themes/workflow_mid/functions.php
+++ wp-content/themes/workflow_mid/functions.php
@@ -13,3 +13,11 @@ function init_view_style()
 }
 
 add_action("wp_enqueue_scripts", "init_view_style");
+
+function init_workflowMid_custom_blocks()
+{
+    register_block_type(__DIR__ . "/build/main-nav");
+    register_block_type(__DIR__ . "/build/features");
+}
+
+add_action("init", "init_workflowMid_custom_blocks");
---
+++ wp-content/themes/workflow_mid/src/features/render.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+?>
+<div <?php echo get_block_wrapper_attributes(); ?> class="wp-block-group featureTile">
+	<figure class="wp-block-image">
+		<img src="http://sp15midworkflow.local/wp-content/uploads/2025/03/n413-midterm-feature.jpg" alt="" />
+	</figure>
+	<div class="wp-block-group">
+		<h3 class="wp-block-heading">Feature Name</h3>
+		<p>
+			Description of the feature and how it enhances the value of the system
+		</p>
+	</div>
+</div>
